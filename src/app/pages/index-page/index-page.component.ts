d9b6eb43ddc976dead9bc4bd1a5badfc44592c48
Genotroid <32167360+Genotroid@users.noreply.github.com>
2023-01-02 19:13:39 +0500

    Fix: change uer tab count in prettier

--- src/app/pages/index-page/index-page.component.ts
+++ src/app/pages/index-page/index-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-index-page',
-  templateUrl: './index-page.component.html',
-  styleUrls: ['./index-page.component.less'],
+    selector: 'app-index-page',
+    templateUrl: './index-page.component.html',
+    styleUrls: ['./index-page.component.less'],
 })
 export class IndexPageComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 }
